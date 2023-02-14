70abeabbdcc629c22962d60ee939535c30dfc16d
philip.graham <philip.graham@accenture.com>
2023-02-14 11:13:45 -0500

    enable CR expansion for nvim-autopairs

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -25,5 +25,12 @@ null_ls.setup {
 	},
 }
 
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+local cmp = require("cmp")
+cmp.event:on(
+	"confirm_done",
+	cmp_autopairs.on_confirm_done()
+)
+
 -- vim.cmd('autocmd BufWritePre *.go LspZeroFormat!')
 -- vim.cmd('autocmd BufWritePre *.ts LspZeroFormat!')
